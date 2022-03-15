b805693a02c6d1a7f3f75bff77466cd4c50be44c
LHD <chilee4650@naver.com>
2022-03-15 19:28:11 +0900

    IDE

---
+++ workspace/TestJava/bin/work/test01/classTest.java.bak
@@ -0,0 +1,13 @@
+
+public class ClassTest
+
+{
+	public static void main(String[] args) 
+	{
+		
+		ClassTest ct = new ClassTest();
+		ct.getClass().getName();
+		
+		
+	}
+}
---
+++ workspace/TestJava/src/t03_07/DataFormatException.java
@@ -0,0 +1,10 @@
+package t03_07;
+
+public class DataFormatException extends RuntimeException{
+	public DataFormatException() {
+		
+	}
+	public DataFormatException(String message){
+		super(message);
+	}
+}
